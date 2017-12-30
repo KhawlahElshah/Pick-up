4b2f8db0574498586ebf8cb75e84a9b699aa4604
Khawlah Elshah <khawlah_t@yahoo.com>
2017-12-30 18:50:09 +0200

    project with no activities

---
+++ app/src/main/java/android/projects/MainActivity.java
@@ -0,0 +1,19 @@
+
+package android.projects;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    // global scope variables
+
+    @Override
+    protected void onCreate(Bundle bnd) {
+        super.onCreate(bnd);
+
+            setContentView(R.layout.login);
+
+        }
+
+}// MainActivity
